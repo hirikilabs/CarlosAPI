77593833fe2587ef595554f2e30265a69f647ce3
David Pello <dvpg@tuta.io>
2024-09-26 10:32:10 +0200

    Added User struct

--- pkg/models/model.go
+++ pkg/models/model.go
@@ -1,17 +1,18 @@
 package models
 
-import(
-	"carlosapi/pkg/database"
+import (
 	"carlosapi/pkg/config"
+	"carlosapi/pkg/database"
 	"fmt"
-	"gorm.io/gorm"
 	"time"
+
+	"gorm.io/gorm"
 )
 
 // strings that represent the status of the recordings
-const(
-	Created = "Created"
-	Running = "Running"
+const (
+	Created  = "Created"
+	Running  = "Running"
 	Recorded = "Recorded"
 	Finished = "Finished"
 )
@@ -20,27 +21,32 @@ var db *gorm.DB
 
 type RecordStatus string
 
-type Recording struct {
-	gorm.Model
-	Id			int64	`json:"id"`            
-	User		string	`json:"user"`
-	Time		int64	`json:"time"`            // unix timestamp
-	Frequency	int 	`json:"frequency"`       // Hz
-	SampleRate	int 	`json:"sample_rate"`     
-	Gain		int 	`json:"gain"`            // integer, real gain/10
-	RecTime		int64	`json:"rec_time"`        // ms
-	WaitTime	int64	`json:"wait_time"`       // ms
-	Az          float64 `json:"az"`
-	El          float64 `json:"el"`
-	AzRange		float64 `json:"az_range"`
-	AzStep		float64 `json:"az_step"`
-	ElRange		float64 `json:"el_range"`
-	ElStep		float64 `json:"el_step"`
-	CalcTime    int64   `json:"calc_time"`       // ms
-	Status		RecordStatus `json:"status"`
+type User struct {
+	Username string
+	Password string
 }
 
-type Notification struct {}
+type Recording struct {
+	gorm.Model
+	Id         int64        `json:"id"`
+	User       string       `json:"user"`
+	Time       int64        `json:"time"`      // unix timestamp
+	Frequency  int          `json:"frequency"` // Hz
+	SampleRate int          `json:"sample_rate"`
+	Gain       int          `json:"gain"`      // integer, real gain/10
+	RecTime    int64        `json:"rec_time"`  // ms
+	WaitTime   int64        `json:"wait_time"` // ms
+	Az         float64      `json:"az"`
+	El         float64      `json:"el"`
+	AzRange    float64      `json:"az_range"`
+	AzStep     float64      `json:"az_step"`
+	ElRange    float64      `json:"el_range"`
+	ElStep     float64      `json:"el_step"`
+	CalcTime   int64        `json:"calc_time"` // ms
+	Status     RecordStatus `json:"status"`
+}
+
+type Notification struct{}
 
 func init() {
 	// connect to the database and create the tables if needed
@@ -48,6 +54,7 @@ func init() {
 	database.ConnectDB(conf.Database)
 	db = database.GetDB()
 	db.AutoMigrate(&Recording{})
+	db.AutoMigrate(&User{})
 }
 
 // add a recording to the database
@@ -63,13 +70,13 @@ func (r *Recording) Update() *Recording {
 }
 
 // calculate estimated time for the recording
-func (r* Recording) EstimateTime() {
+func (r *Recording) EstimateTime() {
 	// (record time + wait time) * number of points
-	r.CalcTime = (int64(r.RecTime) + int64(r.WaitTime)) * int64((r.AzRange / r.AzStep) * (r.ElRange / r.ElStep))
+	r.CalcTime = (int64(r.RecTime) + int64(r.WaitTime)) * int64((r.AzRange/r.AzStep)*(r.ElRange/r.ElStep))
 }
 
 // check recording fields
-func (r* Recording) Check() error {
+func (r *Recording) Check() error {
 	// check time
 	if r.Time <= time.Now().UnixMilli() {
 		return fmt.Errorf("Time is in the past")
@@ -83,7 +90,7 @@ func (r* Recording) Check() error {
 	if r.AzRange < 0 || r.AzStep < 0 || r.ElStep < 0 || r.ElRange < 0 {
 		return fmt.Errorf("Movement ranges and steps can't be negative")
 	}
-	
+
 	return nil
 }
 
@@ -99,7 +106,6 @@ func GetRecordings() []Recording {
 	db.Find(&Recordings)
 	return Recordings
 }
-
 
 // Get a recording by it's ID
 func GetRecordingById(Id int64) (*Recording, *gorm.DB) {
