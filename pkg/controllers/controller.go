57631da5331e6e84185ba1f955f11c88f2121479
David Pello <dvpg@tuta.io>
2024-10-02 15:41:54 +0200

    Show error

--- pkg/controllers/controller.go
+++ pkg/controllers/controller.go
@@ -541,6 +541,7 @@ func WebInfo(writer http.ResponseWriter, request *http.Request) {
 		},
 	}).ParseFiles("html/info.html")
 	if err != nil {
+		log.Println(err.Error())
 		writer.WriteHeader(http.StatusInternalServerError)
 		writer.Write([]byte("Problem loading web page"))
 		writer.Write([]byte(err.Error()))
